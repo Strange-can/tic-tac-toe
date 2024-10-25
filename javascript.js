e58d6a53623fe4effb1f491140a5f99fb38592dd
Strange-can <179990912+Strange-can@users.noreply.github.com>
2024-10-25 22:54:40 +0800

    Add checkStatus() to check if game has ended(won)
    Only added win conditions for player1 at the moment.

--- javascript.js
+++ javascript.js
@@ -11,6 +11,18 @@ const gameBoard = (function () {
 
 const game = ( function () {
     let count = 0
+    function checkStatus () {
+        if ( (boardArray[0] === 'x' && boardArray[1] === 'x' && boardArray[2] === 'x') 
+              || (boardArray[3] === 'x' && boardArray[4] === 'x' && boardArray[5] === 'x') 
+              || (boardArray[6] === 'x' && boardArray[7] === 'x' && boardArray[8] === 'x') 
+              || (boardArray[0] === 'x' && boardArray[3] === 'x' && boardArray[6] === 'x') 
+              || (boardArray[1] === 'x' && boardArray[4] === 'x' && boardArray[7] === 'x') 
+              || (boardArray[2] === 'x' && boardArray[5] === 'x' && boardArray[8] === 'x') 
+              || (boardArray[0] === 'x' && boardArray[4] === 'x' && boardArray[8] === 'x') 
+              || (boardArray[2] === 'x' && boardArray[4] === 'x' && boardArray[6] === 'x') ) {
+            console.log("x wins")
+        }
+    }
     const action = function (position) {
         let positions = '012345678'
         if ( positions.includes(position) && count % 2 === 0 && boardArray[position] === '' ) {
@@ -25,6 +37,7 @@ const game = ( function () {
             console.log(boardArray)
             console.log(count)
             }
+        checkStatus()
     }
 
     return { action }
