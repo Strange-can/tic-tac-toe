3b723dca0f4a353eae1179f2734a758e6e65b61b
Strange-can <179990912+Strange-can@users.noreply.github.com>
2024-10-26 21:53:07 +0800

    Change array scope to be within gameBoard IIFE

--- javascript.js
+++ javascript.js
@@ -1,29 +1,13 @@
-let boardArray = []
 const gameBoard = (function () {
+    let boardArray = []
+
     const setBoard = function () {
     for (let i = 0; i < 9; i++ ) {
         boardArray.push( '' )
     }}
-
-    return {setBoard}
-}
-)()
-
-const game = ( function () {
+    
     let count = 0
-    function checkStatus () {
-        if ( (boardArray[0] === 'x' && boardArray[1] === 'x' && boardArray[2] === 'x') 
-              || (boardArray[3] === 'x' && boardArray[4] === 'x' && boardArray[5] === 'x') 
-              || (boardArray[6] === 'x' && boardArray[7] === 'x' && boardArray[8] === 'x') 
-              || (boardArray[0] === 'x' && boardArray[3] === 'x' && boardArray[6] === 'x') 
-              || (boardArray[1] === 'x' && boardArray[4] === 'x' && boardArray[7] === 'x') 
-              || (boardArray[2] === 'x' && boardArray[5] === 'x' && boardArray[8] === 'x') 
-              || (boardArray[0] === 'x' && boardArray[4] === 'x' && boardArray[8] === 'x') 
-              || (boardArray[2] === 'x' && boardArray[4] === 'x' && boardArray[6] === 'x') ) {
-            console.log("x wins")
-        }
-    }
-    const action = function (position) {
+    const inputAction = function (position) {
         let positions = '012345678'
         if ( positions.includes(position) && count % 2 === 0 && boardArray[position] === '' ) {
             count++
@@ -37,12 +21,46 @@ const game = ( function () {
             console.log(boardArray)
             console.log(count)
             }
-        checkStatus()
+        game.checkStatus()
     }
 
-    return { action }
+    const getBoardArray = function () {
+        return boardArray
+    }
+
+    return { setBoard, inputAction, getBoardArray }
+}
+)()
+
+const game = ( function () {
+    function checkStatus () {
+        const boardArray = gameBoard.getBoardArray()
+
+        if ( (boardArray[0] === 'x' && boardArray[1] === 'x' && boardArray[2] === 'x') 
+              || (boardArray[3] === 'x' && boardArray[4] === 'x' && boardArray[5] === 'x') 
+              || (boardArray[6] === 'x' && boardArray[7] === 'x' && boardArray[8] === 'x') 
+              || (boardArray[0] === 'x' && boardArray[3] === 'x' && boardArray[6] === 'x') 
+              || (boardArray[1] === 'x' && boardArray[4] === 'x' && boardArray[7] === 'x') 
+              || (boardArray[2] === 'x' && boardArray[5] === 'x' && boardArray[8] === 'x') 
+              || (boardArray[0] === 'x' && boardArray[4] === 'x' && boardArray[8] === 'x') 
+              || (boardArray[2] === 'x' && boardArray[4] === 'x' && boardArray[6] === 'x') ) {
+            console.log("x wins")
+        }
+        else if ( (boardArray[0] === 'o' && boardArray[1] === 'o' && boardArray[2] === 'o') 
+              || (boardArray[3] === 'o' && boardArray[4] === 'o' && boardArray[5] === 'o') 
+              || (boardArray[6] === 'o' && boardArray[7] === 'o' && boardArray[8] === 'o') 
+              || (boardArray[0] === 'o' && boardArray[3] === 'o' && boardArray[6] === 'o') 
+              || (boardArray[1] === 'o' && boardArray[4] === 'o' && boardArray[7] === 'o') 
+              || (boardArray[2] === 'o' && boardArray[5] === 'o' && boardArray[8] === 'o') 
+              || (boardArray[0] === 'o' && boardArray[4] === 'o' && boardArray[8] === 'o') 
+              || (boardArray[2] === 'o' && boardArray[4] === 'o' && boardArray[6] === 'o') ) {
+            console.log('o wins')
+        }
+        
+    }
+
+    return { checkStatus }
 }
 )()
 
 gameBoard.setBoard()
-console.log(boardArray)
