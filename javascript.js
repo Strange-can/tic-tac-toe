93e9043854ba71bd8e0209e87f832ac84817c111
Strange-can <179990912+Strange-can@users.noreply.github.com>
2024-11-03 19:02:28 +0800

    Add code to confirm event listener to revert DOM
    Confirm button event listeners will now revert back to the orginal DOM with the new inputted name in place of the default.
    If user presses confirm with a blank input the "new inputted name" will be the previous name.

--- javascript.js
+++ javascript.js
@@ -5,6 +5,9 @@ const gameBoard = (function () {
     let count = 0
 
     const setBoard = function () {
+        boardArray.splice( 0, boardArray.length )
+        gameField.textContent = ''
+        count = 0
         for (let i = 0; i < 9; i++ ) {
             boardArray.push( '' )
             const square = document.createElement("div")
@@ -64,6 +67,7 @@ const game = ( function () {
         
         const player1Score = document.getElementById("player1-score")
         let player1CurrentScore = 0
+        console.log(player1CurrentScore)
 
         const tieScore = document.getElementById("tie-score")
         let tieCurrentScore = 0
@@ -80,6 +84,7 @@ const game = ( function () {
               || (boardArray[0] === 'x' && boardArray[4] === 'x' && boardArray[8] === 'x') 
               || (boardArray[2] === 'x' && boardArray[4] === 'x' && boardArray[6] === 'x') ) {
             console.log("x wins")
+            name1 = customName.getName1()
             scoreMessage.textContent = `${name1} wins!`
             player1CurrentScore++
             player1Score.textContent = player1CurrentScore
@@ -94,6 +99,7 @@ const game = ( function () {
               || (boardArray[0] === 'o' && boardArray[4] === 'o' && boardArray[8] === 'o') 
               || (boardArray[2] === 'o' && boardArray[4] === 'o' && boardArray[6] === 'o') ) {
             console.log('o wins')
+            name2 = customName.getName2()
             scoreMessage.textContent = `${name2} wins!`
             player2CurrentScore++
             player2Score.textContent = player2CurrentScore
@@ -114,21 +120,98 @@ const game = ( function () {
 )()
 
 const customName = ( function () {
-    const player1Name = document.getElementById("player1-name")
+    const player1Section = document.getElementById("player1")
+    let player1Name = document.getElementById("player1-name")
+    let scoreName1 = document.getElementById("score-name1")
+    let name1 = 'player1'
 
-    const player2Name = document.getElementById("player2-name")
-
-    function changePlayer1 (name) {
-    player1Name.textContent = name
+    const player2Section = document.getElementById("player2")
+    let player2Name = document.getElementById("player2-name")
+    let scoreName2 = document.getElementById("score-name2")
+    let name2 = 'player2'
+    
+    function enableNameChange () {
+        const rename1 = document.getElementById("rename1")
+        rename1.addEventListener("click", () => {
+            player1Name.remove()
+            const input1 = document.createElement("input")
+            input1.setAttribute("id", "input1")
+            player1Section.prepend(input1)
+        
+            rename1.remove()
+            const confirm1 =  document.createElement("button")
+            confirm1.textContent = 'CONFIRM'
+            confirm1.addEventListener("click", () => {
+                if (input1.value === '') {
+                    input1.remove()
+                    confirm1.remove()
+                    player1Section.append(player1Name)
+                    player1Section.append(rename1)
+                    return
+                }
+                else {
+                name1 = input1.value
+                scoreName1.textContent = `${name1}(x)`
+                game.checkStatus()
+                player1Name.textContent = name1
+                input1.remove()
+                confirm1.remove()
+                player1Section.append(player1Name)
+                player1Section.append(rename1)
+                }
+            })
+            player1Section.append(confirm1)
+        })
+        
+        const rename2 = document.getElementById("rename2")
+        rename2.addEventListener("click", () => {
+            player2Name.remove()
+            const input2 = document.createElement("input")
+            input2.setAttribute("id", "input2")
+            player2Section.prepend(input2)
+        
+            rename2.remove()
+            const confirm2 =  document.createElement("button")
+            confirm2.textContent = 'CONFIRM'
+            confirm2.addEventListener("click", () => {
+                if (input2.value === '') {
+                    input2.remove()
+                    confirm2.remove()
+                    player2Section.append(player2Name)
+                    player2Section.append(rename2)
+                    return
+                }
+                else {
+                name2 = input2.value
+                scoreName2.textContent = `${name2}(o)`
+                game.checkStatus()
+                player2Name.textContent = name2
+                input2.remove()
+                confirm2.remove()
+                player2Section.append(player2Name)
+                player2Section.append(rename2)
+                }
+            })
+            player2Section.append(confirm2)
+        })
     }
 
-    return { changePlayer1 }
+    function getName1() {
+        return name1
+    }
+
+    function getName2() {
+        return name2
+    }
+
+    return { enableNameChange, getName1, getName2 }
 }
 )()
 
 gameBoard.setBoard()
+customName.enableNameChange()
 
-const player1Section = document.getElementById("player1")
+/*const player1Section = document.getElementById("player1")
 let player1Name = document.getElementById("player1-name")
 let scoreName1 = document.getElementById("score-name1")
 let name1 = 'player1'
@@ -156,7 +239,11 @@ rename1.addEventListener("click", () => {
         name1 = input1.value
         scoreName1.textContent = `${name1}(x)`
         game.checkStatus()
-        //player1Name = name1
+        player1Name.textContent = name1
+        input1.remove()
+        confirm1.remove()
+        player1Section.append(player1Name)
+        player1Section.append(rename1)
         }
     })
     player1Section.append(confirm1)
@@ -180,7 +267,12 @@ rename2.addEventListener("click", () => {
         name2 = input2.value
         scoreName2.textContent = `${name2}(o)`
         game.checkStatus()
+        player2Name.textContent = name2
+        input2.remove()
+        confirm2.remove()
+        player2Section.append(player2Name)
+        player2Section.append(rename2)
         }
     })
     player2Section.append(confirm2)
-})
+})*/
